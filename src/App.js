782bd35f88dc48e3e37aa322d85539e732b8a8b2
Ryhem <Ryhembenahmed@gmail.com>
2022-08-28 22:01:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log("Error");
+      });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.length > 0
+        ? users.map((elt) => <Card key={elt.id} user={elt}></Card>)
+        : "Loading..."}
     </div>
   );
 }
